d8ce511394d7c5804983bf2cfdaadce827dabbaa
Paolo Uy <paolo.uy@obf.ateneo.edu>
2023-08-07 18:56:40 +0800

    Register categoryRoutes

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
 import fastifyPlugin from 'fastify-plugin'
+import { categoryRoutes } from './category/index.js'
 import { operationRoutes } from './operation/index.js'
 
 export default fastifyPlugin((app, options, done) => {
+  app.register(categoryRoutes, { prefix: '/api/v1/categories' })
   app.register(operationRoutes, { prefix: '/api/v1/operations' })
 
   done()
