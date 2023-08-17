694af1c05973eb64e46272a7d95654454a4acc42
Paolo Uy <paolo.uy@obf.ateneo.edu>
2023-08-18 07:04:20 +0800

    Add organization name to lookup

--- src/misc/misc.service.js
+++ src/misc/misc.service.js
@@ -1,10 +1,12 @@
 import { categoryService } from '../category/index.js'
 import { equipmentService } from '../equipment/index.js'
+import { lookupService } from '../lookup/index.js'
 import { operationService } from '../operation/index.js'
 import { workerService } from '../worker/index.js'
 import { workstationService } from '../workstation/index.js'
 
 export const initializeApp = () => {
+  const organizatioName = lookupService.getOne('organizationName')
   const categories = categoryService.getAll()
   const equipments = equipmentService.getAll()
   const operations = operationService.getAll()
@@ -12,6 +14,7 @@ export const initializeApp = () => {
   const workstations = workstationService.getAll()
 
   return {
+    ...organizatioName,
     categories,
     equipments,
     operations,
