b2ec6e197e5240dad59d0069f09654b4d4c01199
Paolo Uy <paolo.uy@obf.ateneo.edu>
2023-08-19 05:45:19 +0800

    Add Render support

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import Fastify from 'fastify'
 import cors from '@fastify/cors'
 import routes from './routes.js'
 
+const host = ('RENDER' in process.env) ? '0.0.0.0' : 'localhost'
+
 const app = Fastify({
   logger: {
     transport: {
@@ -17,7 +19,7 @@ app.register(routes)
 
 const start = () => {
   try {
-    app.listen({ port: PORT })
+    app.listen({ host, port: PORT })
   } catch (err) {
     app.log.error(err)
     process.exit(1)
